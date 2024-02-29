a3c2db7548d54ca3cb6203a1f8d8a9b5b196e6e6
Diana Dzhioeva <dzhioyeva01@mail.ru>
2024-02-29 19:39:55 +0300

    commit tests

--- tests/test_category.py
+++ tests/test_category.py
@@ -11,7 +11,7 @@ def category_shop():
 def test_init(category_shop):
     assert category_shop.name == 'Lime'
     assert category_shop.description == 'Магазин мужской одежды'
-    assert Category.category_count == 1
+    assert Category.category_count == 2
     assert Category.product_count == 0
 
 
@@ -26,3 +26,13 @@ def test_product_list(category_shop):
     assert category_shop.product_list == "Футболка, 2190.5 руб. Остаток: 13 шт."
 
 
+def test_len(category_shop):
+    pr_1 = Product('Футболка', 'хлопковая футболка', 2190.5, 13)
+    category_shop.add_product(pr_1)
+    assert len(category_shop) == 13
+
+
+def test_str(category_shop):
+    pr_1 = Product('Футболка', 'хлопковая футболка', 2190.5, 13)
+    category_shop.add_product(pr_1)
+    assert category_shop.__str__() == "Lime, количество продуктов: 13 шт."
